db55b928c0384d04eee0e4385bc23716a461f02c
Bogdan Krasun <bogdan_krasun@epam.com>
2019-12-23 14:02:27 +0200

    01 Add Spring Boot and properties

---
+++ src/main/java/com/rest/consuming/RestConsumingApp.java
@@ -0,0 +1,13 @@
+package com.rest.consuming;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestConsumingApp {
+
+  public static void main(String[] args) {
+    SpringApplication.run(RestConsumingApp.class, args);
+  }
+
+}
